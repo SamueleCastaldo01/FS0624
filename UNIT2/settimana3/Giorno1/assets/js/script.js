19eb4bbb95b3089f136ed07ab789d7c4f2b294f7
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-07-22 15:29:28 +0200

    commit

--- UNIT2/settimana3/Giorno1/assets/js/script.js
+++ UNIT2/settimana3/Giorno1/assets/js/script.js
@@ -18,3 +18,39 @@ const u1 = new User ("Samuele", "Castaldo", "23", "Napoli");
 const u2 = new User ("Antonio", "Angelino", "21", "Napoli");
 console.log(u1)
 u1.confEta(u2)
+
+//Secondo Esercizio
+// Creare la classe Pet
+class Pet {
+    constructor(petName, ownerName, species, breed) {
+        this.petName = petName;
+        this.ownerName = ownerName;
+        this.species = species;
+        this.breed = breed;
+    }
+}
+
+const form1 = document.getElementById("form1");
+
+form1.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Recuperiamo i dati
+    const petNameInput = document.getElementById("petName");
+    const ownerNameInput = document.getElementById("ownerName");
+    const speciesInput = document.getElementById("species");
+    const breedInput = document.getElementById("breed");
+
+    // Recuperiamo i valori
+    const petNameValue = petNameInput.value;
+    const ownerNameValue = ownerNameInput.value;
+    const speciesValue = speciesInput.value;
+    const breedValue = breedInput.value;
+
+    // Creiamo un oggetto Pet
+    const pet = new Pet(petNameValue, ownerNameValue, speciesValue, breedValue);
+
+    // Visualizziamo l'oggetto Pet nella console
+    console.log("Ecco il mio elemento:", pet);
+
+});
