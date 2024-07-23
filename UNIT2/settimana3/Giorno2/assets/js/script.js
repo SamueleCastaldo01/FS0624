4598a200fa63b3ff42a492f96c771c512bf5b1b0
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-07-23 14:29:36 +0200

    commit

--- UNIT2/settimana3/Giorno2/assets/js/script.js
+++ UNIT2/settimana3/Giorno2/assets/js/script.js
@@ -1,6 +1,37 @@
 const nome = document.getElementById("name");
 const nomeValore = document.getElementById("nomeValore")
 
-let nomeP = localStorage.getItem("nome")
-const nameValue = nome.value;
+const contatore = document.getElementById("contatore")
+let contValue = sessionStorage.getItem("contatore") || 0; //vado a predere il valore attuale, altrimenti inizia da 0
 
+let nomeP = localStorage.getItem("nome") || "";  //controllo
+nomeValore.innerText = nomeP;
+
+console.log(localStorage.getItem("nome"))
+
+function saveButton() {
+    const nameValue = nome.value; //va a prendere il valore
+    
+    localStorage.setItem("nome", nameValue)   //lo salva localmente
+    nomeValore.innerText = nameValue;
+    nome.value = ""
+}
+
+function deleteButton() {
+    localStorage.setItem("nome", ""); //andiamo a metterlo vuoto
+    nomeValore.innerText = ""
+}
+
+
+//contatore
+function startCounter() {
+    intervalId = setInterval(() => {
+        contValue++;                 //aggiorna
+        contatore.innerText = contValue;   //DOM
+        sessionStorage.setItem("contatore", contValue);  //aggiorna lo storage
+    }, 1000); // Aggiorna ogni secondo (1000 millisecondi)
+}
+
+
+// Avvia il contatore non appena la pagina è completamente caricata
+window.onload = startCounter;
