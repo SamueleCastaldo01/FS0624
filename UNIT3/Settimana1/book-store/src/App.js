ff223b1e8244aa8516f1fbc445e78e082597635f
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-20 15:03:52 +0200

    commit footer

--- UNIT3/Settimana1/book-store/src/App.js
+++ UNIT3/Settimana1/book-store/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Mynav } from './components/MyNav';
+import { Myfooter } from './components/Myfooter';
 
 function App() {
   return (
     <div className="App">
       <Mynav />
+
+      <Myfooter />
     </div>
   );
 }
---
+++ UNIT3/Settimana1/book-store/src/components/Myfooter.js
@@ -0,0 +1,10 @@
+export function Myfooter() {
+
+    return(
+        <>
+            <div className="bg-dark text-white position-absolute bottom-0 w-100">
+                <p>Footer</p>
+            </div>
+        </>
+    )
+}
