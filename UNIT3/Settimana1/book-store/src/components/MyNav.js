ea6bfd0e41b8667f8978fc57c08567f8f5e3aed9
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-20 14:57:23 +0200

    commit

---
+++ UNIT3/Settimana1/book-store/src/components/MyNav.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export function Mynav() {
+    return(
+        <>
+    <Navbar collapseOnSelect expand="md" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">Book Store</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        </>
+    )
+}
