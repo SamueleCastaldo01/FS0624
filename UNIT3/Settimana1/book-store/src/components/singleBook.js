f464f65b366562a762582c3fe1cd6193c13f49b8
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-21 15:57:29 +0200

    commit

--- UNIT3/Settimana1/book-store/src/components/singleBook.js
+++ UNIT3/Settimana1/book-store/src/components/singleBook.js
@@ -6,22 +6,28 @@ export function SingleBook(props) {
 
     const [selected, setSelected] = useState(false);
 
-    handlerSelected = () => {
+    const handlerSelected = () => {
         setSelected(!selected)
     }
 
     return(
         <>
-            <Card key={props.asin} className='mt-4' style={{ width: '18rem' }} onClick={handlerSelected}>
+        <Card
+            key={props.asin}
+            className="mt-4"
+            style={{
+                width: '18rem',
+                border: selected ? '2px solid red' : '1px solid #ddd',
+            }}
+            onClick={handlerSelected}
+        >
             <Card.Img variant="top" src={props.img} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
-                <Card.Text>
-                ${props.price}
-                </Card.Text>
+                <Card.Text>${props.price}</Card.Text>
                 <Button variant="primary">Compra</Button>
             </Card.Body>
-            </Card>
+        </Card>
         </>
     )
 }
