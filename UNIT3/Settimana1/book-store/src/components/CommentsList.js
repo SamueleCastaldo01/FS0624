2dc27197a79215e20f2721191cb5ce9b0a2625b0
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-22 16:00:31 +0200

    commit

--- UNIT3/Settimana1/book-store/src/components/CommentsList.js
+++ UNIT3/Settimana1/book-store/src/components/CommentsList.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import SingleComment from "./SingleComment";
+import { Alert } from "react-bootstrap";
 import CircularProgress from '@mui/material/CircularProgress';
 
 class CommentList extends Component {
@@ -15,6 +16,12 @@ class CommentList extends Component {
             {this.props.isLoading &&
             <CircularProgress /> 
             }
+            {this.props.isError && (
+                <Alert variant="danger">
+                  Oops! Qualcosa è andato storto 😱
+                </Alert>     
+            )}
+
                 {this.props.comments.map((comment)=> {
                     return(
                         <SingleComment comment={comment.comment} rate={comment.rate}/>
