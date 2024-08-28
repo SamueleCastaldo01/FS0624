75b0eccd1c07b9c6d848a25e7ac4b7264dd6c1d6
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-28 11:52:25 +0200

    commit

--- UNIT3/Settimana1/book-store/src/components/SingleComment.js
+++ UNIT3/Settimana1/book-store/src/components/SingleComment.js
@@ -1,36 +1,35 @@
 function SingleComment(props) {
-
   const handleDelete = (commentId) => {
     fetch(`https://striveschool-api.herokuapp.com/api/comments/${commentId}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MmZhZTQzYTU2ODAwMTU4ZWMzZDMiLCJpYXQiOjE3MjQzMjk5MDMsImV4cCI6MTcyNTUzOTUwM30.s1jeWc-FEqZ5QaFDvzmqrtKBMPEXKi8Lmut4MZbfvV4",
-      }
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MmZhZTQzYTU2ODAwMTU4ZWMzZDMiLCJpYXQiOjE3MjQzMjk5MDMsImV4cCI6MTcyNTUzOTUwM30.s1jeWc-FEqZ5QaFDvzmqrtKBMPEXKi8Lmut4MZbfvV4",
+      },
     })
       .then((response) => {
         if (response.ok) {
           //eliminato con sucesso
           props.onCommentDeleted(); // aggiorna la lista dei commenti
         } else {
-          alert('Errore nella cancellazione del commento, riprova più tardi.');
-          throw new Error('Errore durante la cancellazione');
+          alert("Errore nella cancellazione del commento, riprova più tardi.");
+          throw new Error("Errore durante la cancellazione");
         }
       })
       .catch((err) => {
-        alert('Errore di rete: ' + err.message);
+        alert("Errore di rete: " + err.message);
       });
-  }
+  };
 
-
-    return (
-      <>
-        <div className="d-flex justify-content-between" key={props._id}>
-          <p>{props.comment}</p>
-          <p>{props.rate}</p>
-          <button onClick={() => handleDelete(props._id)}>delete</button>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="d-flex justify-content-between" key={props._id}>
+        <p>{props.comment}</p>
+        <p>{props.rate}</p>
+        <button onClick={() => handleDelete(props._id)}>delete</button>
+      </div>
+    </>
+  );
 }
 
 export default SingleComment;
--- UNIT3/Settimana1/book-store/src/components/singleBook.js
+++ UNIT3/Settimana1/book-store/src/components/singleBook.js
@@ -1,26 +1,25 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export function SingleBook({ title, img, price, asin, onSelect, isSelected }) {
-
-    return(
-        <div>
-            <Card
-                key={asin}
-                className="mt-4"
-                style={{
-                    width: '18rem',
-                    border: isSelected ? '2px solid red' : '1px solid #ddd',
-                }}
-                onClick={() => onSelect(asin)} // Notifica la selezione al componente padre
-            >
-                <Card.Img variant="top" src={img} />
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>${price}</Card.Text>
-                    <Button variant="primary">Compra</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    )
+  return (
+    <div>
+      <Card
+        key={asin}
+        className="mt-4"
+        style={{
+          width: "18rem",
+          border: isSelected ? "2px solid red" : "1px solid #ddd",
+        }}
+        onClick={() => onSelect(asin)} // Notifica la selezione al componente padre
+      >
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>${price}</Card.Text>
+          <Button variant="primary">Compra</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
