bc0a3246900b8cedd50a84f24b2e2f4103d32d4a
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-03 15:14:32 +0200

    commit

--- UNIT3/Settimana3/d9-hw-start/src/redux/store/index.js
+++ UNIT3/Settimana3/d9-hw-start/src/redux/store/index.js
@@ -1,14 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import FavoritesRed from '../reducers/favorites'; // Importa automaticamente l'index.js
+import FavoritesRed from '../reducers/favorites';
 import JobsList from '../reducers/jobsList';
 
 const bigReducer = combineReducers({
-  favorites: FavoritesRed, // Combina i reducer sotto la chiave 'favorites'
+  favorites: FavoritesRed, 
   jobs: JobsList,
 });
 
 const store = configureStore({
-  reducer: bigReducer, // Usa bigReducer come reducer principale
+  reducer: bigReducer, 
 });
 
 export default store;
