6df1e6aa30879a4406e3ad7e33ceeba9821ef586
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-25 00:44:44 +0200

    commit

--- UNIT4/Settimana1/Esercitazione 2/src/Main.java
+++ UNIT4/Settimana1/Esercitazione 2/src/Main.java
@@ -1,8 +1,62 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Proviamo");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci un numero da 0 a 3: ");
+        String numero = scanner.nextLine();
+
+        switch (numero) {
+            case "0":
+                System.out.println("zero");
+                break;
+            case "1":
+                System.out.println("uno");
+                break;
+            case "2":
+                System.out.println("due");
+                break;
+            case "3":
+                System.out.println("tre");
+                break;
+            default:
+                System.out.println("Errore nell'inserimento");
+                break;
+        }
+
+        //Esercizio 3
+        String s = "";
+        while(!s.equals(":q")) {
+            String conc = "";
+            System.out.print("Inserisci una stringa: ");
+             s = scanner.nextLine();
+            for(int i =0; i < s.length() ; i++) {
+                conc += s.charAt(i) + ",";
+            }
+            System.out.println(conc);
+        }
+
     }
 
+    public static boolean stringaParioDispari(String s) {
+        if(s.length() % 2 == 0 ) {  //lunghezza pari
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean annoBisestile(int n) {
+        if(n % 4 == 0) {
+            return true;
+        } else if (n % 100 == 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
 
 }
