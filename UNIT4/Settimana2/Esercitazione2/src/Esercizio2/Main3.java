cb6fab6fccf0a7735e70e18718aaad79c4773dd0
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-01 21:03:31 +0200

    esercizio 2 3

--- UNIT4/Settimana2/Esercitazione2/src/Esercizio2/Main3.java
+++ UNIT4/Settimana2/Esercitazione2/src/Esercizio2/Main3.java
@@ -21,28 +21,48 @@ public class Main3 {
             System.out.println(i);
         }
 
+        pariDispariList(random, true);
+
 
     }
 
     public static ArrayList<Integer> randomList(int n) {
         ArrayList<Integer> lista = new ArrayList<>();
-
         for(int i = 0; i < n; i++) { //inserisci n numeri casuali all'interno della lista
             int numeroCasuale = (int) (Math.random() * 101);
             lista.add(numeroCasuale);
         }
-
         //ordiniamo la lista
         Collections.sort(lista);
-
         return lista;
     }
 
     public static ArrayList<Integer> reverseList(ArrayList<Integer> lista) {
-
         //ordiniamo la lista in maniera inversa
         lista.sort(Collections.reverseOrder());
-
         return lista;
     }
+
+    public static void pariDispariList(ArrayList<Integer> lista, boolean b) {
+        System.out.println("Terza funzione--------------------");
+        //ordiniamo la lista in maniera inversa
+        if(b) {
+            System.out.println("Numeri pari: ");
+            for(int i : lista) {
+                if(i % 2 == 0) { //se è pari me lo stampi
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Numeri dispari: ");
+            for(int i : lista) {
+                if(i % 2 != 0) { //sono i dispari
+                    System.out.println(i);
+                }
+            }
+        }
+
+    }
+
+
 }
