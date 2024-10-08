7d3638f3ca975189cbdc6876e19fd7a45949d690
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-08 15:27:10 +0200

    perfetto creata l'entità e funziona correttamente

--- UNIT4/Settimana3/GestioneEventi/src/main/java/samueleCastaldo/Application.java
+++ UNIT4/Settimana3/GestioneEventi/src/main/java/samueleCastaldo/Application.java
@@ -3,13 +3,17 @@ package samueleCastaldo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import samueleCastaldo.entities.Evento;
+import samueleCastaldo.entities.EventoType;
+
+import java.time.LocalDate;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
 
-        System.out.println("Hello World!");
+
     }
 }
