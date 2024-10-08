63c48041746bcf0dbd3d5f034cf7726a70f10ce1
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-08 14:58:19 +0200

    commit

--- UNIT4/Settimana3/GestioneEventi/src/main/java/samueleCastaldo/Application.java
+++ UNIT4/Settimana3/GestioneEventi/src/main/java/samueleCastaldo/Application.java
@@ -1,8 +1,15 @@
 package samueleCastaldo;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
+        EntityManager em = emf.createEntityManager();
+
         System.out.println("Hello World!");
     }
 }
