b4590cd2555c27ac611d1174dd0dc129c5da8560
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-10 15:43:39 +0200

    commit

---
+++ UNIT4/Settimana3/giorno5/src/main/java/samueleCastaldo/Application.java
@@ -0,0 +1,14 @@
+package samueleCastaldo;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("settimana3giorno4");
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        System.out.println("Hello World!");
+    }
+}
