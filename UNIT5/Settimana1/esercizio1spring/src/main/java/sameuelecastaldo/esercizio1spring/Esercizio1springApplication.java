b1b5f193b6469e767f348f2f6f62bf81054be681
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-21 22:54:15 +0200

    commit

---
+++ UNIT5/Settimana1/esercizio1spring/src/main/java/sameuelecastaldo/esercizio1spring/Esercizio1springApplication.java
@@ -0,0 +1,13 @@
+package sameuelecastaldo.esercizio1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio1springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio1springApplication.class, args);
+	}
+
+}
---
+++ UNIT5/Settimana1/esercizio1spring/src/test/java/sameuelecastaldo/esercizio1spring/Esercizio1springApplicationTests.java
@@ -0,0 +1,13 @@
+package sameuelecastaldo.esercizio1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio1springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
