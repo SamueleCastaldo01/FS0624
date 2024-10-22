5e31a01a890db76cd52ae6f90011ecedbd1530c1
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-22 23:36:30 +0200

    commit giorno2

---
+++ UNIT5/Settimana1/giorno2/src/test/java/samuelecastaldo/giorno2/Giorno2ApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelecastaldo.giorno2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Giorno2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
