eabcc7afb0eec1b43439173d431b70a6ac587be9
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-24 09:30:05 +0200

    commit

---
+++ UNIT5/Settimana1/provadb/src/main/java/samuelecastaldo/provadb/ProvadbApplication.java
@@ -0,0 +1,14 @@
+package samuelecastaldo.provadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvadbApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProvadbApplication.class, args);
+	}
+
+}
---
+++ UNIT5/Settimana1/provadb/src/test/java/samuelecastaldo/provadb/ProvadbApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelecastaldo.provadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
