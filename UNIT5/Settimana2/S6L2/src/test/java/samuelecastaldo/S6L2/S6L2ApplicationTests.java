e40afbcc825ed63e50819d8fed4ba9f2f9ef6816
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-29 14:46:59 +0100

    commit

---
+++ UNIT5/Settimana2/S6L2/src/test/java/samuelecastaldo/S6L2/S6L2ApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelecastaldo.S6L2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6L2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
